51e4b692666b2db7a892ca3007e2d82f48bdfed9
anis bastide <anis.bastide@upslide.net>
2019-12-10 17:00:40 +0100

    added classes Priest and Thief

--- ProjetPOO/src/com/company/Priest.java
+++ ProjetPOO/src/com/company/Priest.java
@@ -1,4 +1,24 @@
 package com.company;
 
-public class Priest {
+import java.util.Scanner;
+
+public class Priest extends Character {
+    private int heal;
+    private int maxHealth=healthPoint;
+    public Priest(){
+        Scanner userInput=new Scanner(System.in);
+        System.out.println("Give me your heal:");
+        heal=userInput.nextInt();
+    }
+    public void Damage(int damage){
+        healthPoint=healthPoint - damage;
+        System.out.println();
+        if (healthPoint+heal>maxHealth) {
+            healthPoint = maxHealth;
+        }
+        else{
+            healthPoint=healthPoint+heal;
+            System.out.println(name + " heal himself for " + heal + "HP.");
+        }
+    }
 }
--- ProjetPOO/src/com/company/Thief.java
+++ ProjetPOO/src/com/company/Thief.java
@@ -1,4 +1,37 @@
 package com.company;
 
-public class Thief {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Thief extends Character {
+    private int dodge;
+    private int critical;
+    public Thief(){
+        System.out.println("Give me your dodge /100");
+        Scanner userInput=new Scanner(System.in);
+        dodge=userInput.nextInt();
+        System.out.println("Give me your critical");
+        userInput=new Scanner(System.in);
+        critical=userInput.nextInt();
+    }
+    int baseDamage = damage;
+    public int GetDamage(){
+        var rand2 = new Random().nextInt(100);
+        if (rand2 <= critical){
+            damage = baseDamage * 2;
+            return damage;
+        }
+        else {
+            return damage;
+        }
+    }
+    public void Damage(int damage){
+        var rand1 = new Random().nextInt(100);
+        if (rand1 <= dodge){
+            System.out.println("");
+        }
+        else {
+            healthPoint=healthPoint - damage;
+        }
+    }
 }
